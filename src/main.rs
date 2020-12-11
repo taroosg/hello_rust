d7240a672462772b50b830fdc50a48d3f94cfb33
taroosg <root@taroosg.dev>
2020-12-11 16:06:35 +0900

    add loop

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,27 @@ fn main() {
 
   println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
 
-  println!("Please input your guess.");
-  let mut guess = String::new();
+  loop {
+    println!("Please input your guess.");
+    let mut guess = String::new();
 
-  io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line");
 
-  // 右のguessは入力された文字列の値．trim()は前後の空白と改行を削除する．
-  // u32型は32ビットの非負整数．u32型は小さな非負整数のデフォルトの選択肢として丁度良い
-  // parse()はResultを返す．Okだったら当該数値を返し，Errだったらexpectで文字列を表示し，クラッシュさせる．
-  let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    // 右のguessは入力された文字列の値．trim()は前後の空白と改行を削除する．
+    // u32型は32ビットの非負整数．u32型は小さな非負整数のデフォルトの選択肢として丁度良い
+    // parse()はResultを返す．Okだったら当該数値を返し，Errだったらexpectで文字列を表示し，クラッシュさせる．
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-  println!("You guessed: {}", guess);
-  match guess.cmp(&secret_number) {
-    Ordering::Less => println!("Too small!"),  //小さすぎ！
-    Ordering::Greater => println!("Too big!"), //大きすぎ！
-    Ordering::Equal => println!("You win!"),   //やったね！
+    println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),  //小さすぎ！
+      Ordering::Greater => println!("Too big!"), //大きすぎ！
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      } //やったね！
+    }
   }
 }
