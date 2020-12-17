7ba28c32c0bbeec1e1a72c0b910a91a3d2c93128
taroosg <root@taroosg.dev>
2020-12-17 16:34:48 +0900

    add fibonacci

--- src/main.rs
+++ src/main.rs
@@ -71,12 +71,28 @@ fn main() {
   //   println!("The month is: {}", elm);
   // }
 
-  // 関数呼出し
-  let hoge = another_function(10);
-  println!("number is {}", hoge);
+  // // 関数呼出し
+  // let hoge = another_function(10);
+  // println!("number is {}", hoge);
+  // let number = 6;
+
+  // let result = if number % 4 == 0 {
+  //   // 数値は4で割り切れます
+  //   "number is divisible by 4"
+  // } else if number % 3 == 0 {
+  //   // 数値は3で割り切れます
+  //   "number is divisible by 3"
+  // } else if number % 2 == 0 {
+  //   // 数値は2で割り切れます
+  //   "number is divisible by 2"
+  // } else {
+  //   // 数値は4、3、2で割り切れません
+  //   "number is not divisible by 4, 3, or 2"
+  // };
+  // println!("result is {}", result);
 }
 
-// 外部関数定義
-fn another_function(x: i32) -> i32 {
-  x * 2
-}
+// // 外部関数定義
+// fn another_function(x: i32) -> i32 {
+//   x * 2
+// }
